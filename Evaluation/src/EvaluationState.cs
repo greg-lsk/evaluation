e55cc6b5e6a7a8db75e87827586601b594abab72
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-04 04:10:37 +0200

    new:: EvaluationState enum

---
+++ Evaluation/src/EvaluationState.cs
@@ -0,0 +1,10 @@
+﻿namespace Evaluation;
+
+internal enum EvaluationState
+{
+    Uninitialized = 0,
+    Pending    = 1 << 1,
+    Determined = 1 << 2,
+    True       = 1 << 3,
+    False      = 1 << 4 
+}
