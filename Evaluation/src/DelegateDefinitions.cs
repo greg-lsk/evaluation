62433bed554ec329fbb689a7e40c2bc589d02ce9
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-05 08:38:03 +0200

    refactor:: naming changes

--- Evaluation/src/DelegateDefinitions.cs
+++ Evaluation/src/DelegateDefinitions.cs
@@ -7,17 +7,17 @@ public class DelegateDefinitions
 {
     public delegate bool Rule();
     public delegate Task<bool> RuleAsync();
-    public delegate ValueTask<bool> RulePerfAsync();
+    public delegate ValueTask<bool> RuleMaybeAsync();
 
     public delegate bool Rule<T>(T data) where T : class;
     public delegate Task<bool> RuleAsync<T>(T data) where T : class;
-    public delegate ValueTask<bool> RulePerfAsync<T>(T data) where T : class;
+    public delegate ValueTask<bool> RuleMaybeAsync<T>(T data) where T : class;
 
-    public delegate bool ValueRule<T>(in T data) where T : struct;
-    public delegate Task<bool> ValueRuleAsync<T>(T data) where T : struct;
-    public delegate ValueTask<bool> ValueRulePerfAsync<T>(T data) where T : struct;
+    public delegate bool RuleOnStruct<T>(in T data) where T : struct;
+    public delegate Task<bool> RuleOnStructAsync<T>(T data) where T : struct;
+    public delegate ValueTask<bool> RuleOnStructMaybeAsync<T>(T data) where T : struct;
 
-    public delegate bool ViewRule<T>(View<T> data) where T : struct;
-    public delegate Task<bool> ViewRuleAsync<T>(View<T> data) where T : struct;
-    public delegate ValueTask<bool> ViewRulePerfAsync<T>(View<T> data) where T : struct;
+    public delegate bool RuleOnView<T>(View<T> data) where T : struct;
+    public delegate Task<bool> RuleOnViewAsync<T>(View<T> data) where T : struct;
+    public delegate ValueTask<bool> RuleOnViewMaybeAsync<T>(View<T> data) where T : struct;
 }
