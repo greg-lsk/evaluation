d72a0968a0f36166d2f98d153cc7c95b1ae82040
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-05 08:54:32 +0200

    refactor:: use custom defined delegates for rule arguments
    > Func<...> arguments are replaced with custom Rule...<T> delegates

--- Evaluation/src/IRuleRunner.cs
+++ Evaluation/src/IRuleRunner.cs
@@ -5,11 +5,11 @@ namespace Evaluation;
 
 public interface IRuleRunner<TEvaluation> where TEvaluation : struct, IEvaluator
 {
-    internal TEvaluation Run(Func<bool> rule, Operation operation);
-    internal TEvaluation Run<T>(Func<T, bool> rule, T data, Operation operation);
-
-    internal ValueTask<TEvaluation> RunAsync(Func<Task<bool>> rule, Operation operation);
-    internal ValueTask<TEvaluation> RunAsync<T>(Func<T, Task<bool>> rule, T data, Operation operation);
+    internal TEvaluation Run(Rule rule, Operation operation);
+    internal ValueTask<TEvaluation> RunAsync(RuleAsync rule, Operation operation);
+    
+    internal TEvaluation Run<T>(Rule<T> rule, T data, Operation operation) where T : class;
+    internal ValueTask<TEvaluation> RunAsync<T>(RuleAsync<T> rule, T data, Operation operation) where T : class;
 
     internal TEvaluation Terminate();
 }
--- Evaluation/src/ITriggerHub.cs
+++ Evaluation/src/ITriggerHub.cs
@@ -2,8 +2,10 @@
 
 public interface ITriggerHub
 {
-    public ITriggerHub Register(Delegate rule, Action trigger);
-    public ITriggerHub Register(Func<bool> rule, Action trigger);
-    public ITriggerHub Register<T>(Func<T, bool> rule, Action trigger);
-    public ITriggerHub Register<T>(Func<T, bool> rule, Action<T> trigger);
+    //public ITriggerHub Register(Delegate rule, Action trigger);
+
+    public ITriggerHub Register(Rule rule, Action trigger);
+
+    public ITriggerHub Register<T>(Rule<T> rule, Action trigger) where T : class;
+    public ITriggerHub Register<T>(Rule<T> rule, Action<T> trigger) where T : class;
 }
