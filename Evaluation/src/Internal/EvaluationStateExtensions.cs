6f8d2cf4b3491e8c19327c3e7709d732101b3220
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-05 08:04:14 +0200

    new:: extension methods for EvaluationState

---
+++ Evaluation/src/Internal/EvaluationStateExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Evaluation.Internal;
+
+internal static class EvaluationStateExtensions
+{
+    internal static bool Is(this EvaluationState currentState, EvaluationState state) => ((currentState & state) == state);
+    internal static bool IsInitialized(this EvaluationState currentState) => currentState != EvaluationState.Uninitialized;
+    
+}
