ecf1c81ce935a804ad2b9548dfa81ea231fd750e
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-05 07:37:06 +0200

    new:: add DelegateDefintions class
    holds definitions of supported 'rule Types'

---
+++ Evaluation/src/DelegateDefinitions.cs
@@ -0,0 +1,23 @@
+﻿using View;
+
+
+namespace Evaluation;
+
+public class DelegateDefinitions
+{
+    public delegate bool Rule();
+    public delegate Task<bool> RuleAsync();
+    public delegate ValueTask<bool> RulePerfAsync();
+
+    public delegate bool Rule<T>(T data) where T : class;
+    public delegate Task<bool> RuleAsync<T>(T data) where T : class;
+    public delegate ValueTask<bool> RulePerfAsync<T>(T data) where T : class;
+
+    public delegate bool ValueRule<T>(in T data) where T : struct;
+    public delegate Task<bool> ValueRuleAsync<T>(T data) where T : struct;
+    public delegate ValueTask<bool> ValueRulePerfAsync<T>(T data) where T : struct;
+
+    public delegate bool ViewRule<T>(View<T> data) where T : struct;
+    public delegate Task<bool> ViewRuleAsync<T>(View<T> data) where T : struct;
+    public delegate ValueTask<bool> ViewRulePerfAsync<T>(View<T> data) where T : struct;
+}
