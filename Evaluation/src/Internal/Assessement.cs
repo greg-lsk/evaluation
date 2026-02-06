f549ba86b29834761b1c4ba9ba2cac770619da39
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-06 13:01:13 +0200

    new:: result reporting mechanism
    > IAssessment: Internal contract of what is reported.
    > Asseseement: Internal concrete implementation of IAssessment.
    > Report: Publicly exposed type.

---
+++ Evaluation/src/Internal/Assessement.cs
@@ -0,0 +1,28 @@
+﻿using View;
+
+
+namespace Evaluation.Internal;
+
+internal readonly struct Assessment<T> : IAssessment<T>
+{
+    private readonly View<T> _dataView;
+
+    public Delegate Rule { get; }
+    public ref readonly T Data => ref _dataView.Peek();
+
+    public int CallerLineNumber { get; }
+    public string CallerFilePath { get; }
+
+    public (DateTime Start, DateTime Finish) Timespan { get; }
+
+    public T DetachData() => _dataView.Read();
+
+    public Assessment(Delegate rule, ref T data, DateTime start, DateTime finish, int line, string filePath)
+    {
+        Rule = rule;
+        _dataView = View<T>.Of(ref data);
+        Timespan = (start, finish);
+        CallerLineNumber = line;
+        CallerFilePath = filePath;
+    }
+}
---
+++ Evaluation/src/Internal/IAssessment.cs
@@ -0,0 +1,14 @@
+﻿namespace Evaluation.Internal;
+
+internal interface IAssessment<T>
+{
+    internal Delegate Rule { get; }
+    internal ref readonly T Data { get; }
+
+    internal int CallerLineNumber { get; }
+    internal string CallerFilePath { get; }
+
+    internal (DateTime Start, DateTime Finish) Timespan { get; }
+
+    internal T DetachData();
+}
