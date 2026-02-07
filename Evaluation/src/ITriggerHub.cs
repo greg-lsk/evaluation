0eb08a77838e8564ac85ff9b0c879ef270eb27ba
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-07 20:37:56 +0200

    update:: applied missed Rule -> Check renaming

--- Evaluation/src/ITriggerHub.cs
+++ Evaluation/src/ITriggerHub.cs
@@ -4,8 +4,8 @@ public interface ITriggerHub
 {
     //public ITriggerHub Register(Delegate rule, Action trigger);
 
-    public ITriggerHub Register(Rule rule, Action trigger);
+    public ITriggerHub Register(Check check, Action trigger);
 
-    public ITriggerHub Register<T>(Rule<T> rule, Action trigger) where T : class;
-    public ITriggerHub Register<T>(Rule<T> rule, Action<T> trigger) where T : class;
+    public ITriggerHub Register<T>(Check<T> check, Action trigger) where T : class;
+    public ITriggerHub Register<T>(Check<T> check, Action<T> trigger) where T : class;
 }
