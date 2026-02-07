2de05a3c29b9c06147b9e3be034dc920ae7a672e
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-07 13:09:07 +0200

    new:: IEvaluationStateHandler<T>
    > Iterface that defines the handling of the evaluation state.

---
+++ Evaluation/src/IEvaluationStateHandler.cs
@@ -0,0 +1,12 @@
+﻿using Evaluation.Internal;
+
+
+namespace Evaluation;
+
+public interface IEvaluationStateHandler<T> where T : struct, IEvaluator
+{
+    internal T Terminate();
+    internal T WithState(EvaluationState state);
+
+    internal EvaluationState DetermineNextState(bool evaluatedCheckResult);
+}
