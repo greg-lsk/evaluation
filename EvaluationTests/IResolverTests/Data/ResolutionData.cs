40a736613c06c00b5d972fe7b41a1566159ab828
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-07 23:42:13 +0200

    new:: tests and data for 'IResolver'

---
+++ EvaluationTests/IResolverTests/Data/ResolutionData.cs
@@ -0,0 +1,49 @@
+﻿using Evaluation.Internal;
+
+
+namespace Evaluation.Tests.IResolverTests.Data;
+
+internal class ResolutionData
+{
+    internal static IEnumerable<object[]> BooleanResolutions =
+    [
+        [(EvaluationState.Uninitialized, CheckResult:true, Operation.Or,   currentResult:true,  ResolvesTo:true)],
+        [(EvaluationState.Uninitialized, CheckResult:true, Operation.Or,   currentResult:false, ResolvesTo:false)],
+        [(EvaluationState.Uninitialized, CheckResult:true, Operation.And,  currentResult:true,  ResolvesTo:true)],
+        [(EvaluationState.Uninitialized, CheckResult:true, Operation.And,  currentResult:false, ResolvesTo:false)],
+        [(EvaluationState.Uninitialized, CheckResult:true, Operation.Must, currentResult:true,  ResolvesTo:true)],
+        [(EvaluationState.Uninitialized, CheckResult:true, Operation.Must, currentResult:false, ResolvesTo:false)],
+
+        [(EvaluationState.Uninitialized, CheckResult:false, Operation.Or,   currentResult:true,  ResolvesTo:true)],
+        [(EvaluationState.Uninitialized, CheckResult:false, Operation.Or,   currentResult:false, ResolvesTo:false)],
+        [(EvaluationState.Uninitialized, CheckResult:false, Operation.And,  currentResult:true,  ResolvesTo:true)],
+        [(EvaluationState.Uninitialized, CheckResult:false, Operation.And,  currentResult:false, ResolvesTo:false)],
+        [(EvaluationState.Uninitialized, CheckResult:false, Operation.Must, currentResult:true,  ResolvesTo:true)],
+        [(EvaluationState.Uninitialized, CheckResult:false, Operation.Must, currentResult:false, ResolvesTo:false)],
+
+        [(EvaluationState.Pending | EvaluationState.True, CheckResult:true, Operation.Or,   currentResult:true,  ResolvesTo:true || true)],
+        [(EvaluationState.Pending | EvaluationState.True, CheckResult:true, Operation.Or,   currentResult:false, ResolvesTo:true || false)],
+        [(EvaluationState.Pending | EvaluationState.True, CheckResult:true, Operation.And,  currentResult:true,  ResolvesTo:true && true)],
+        [(EvaluationState.Pending | EvaluationState.True, CheckResult:true, Operation.And,  currentResult:false, ResolvesTo:true && false)],
+        [(EvaluationState.Pending | EvaluationState.True, CheckResult:true, Operation.Must, currentResult:true,  ResolvesTo:true && true)],
+        [(EvaluationState.Pending | EvaluationState.True, CheckResult:true, Operation.Must, currentResult:false, ResolvesTo:true && false)],
+
+        [(EvaluationState.Pending | EvaluationState.False, CheckResult:false, Operation.Or,   currentResult:true,  ResolvesTo:false || true)],
+        [(EvaluationState.Pending | EvaluationState.False, CheckResult:false, Operation.Or,   currentResult:false, ResolvesTo:false || false)],
+        [(EvaluationState.Pending | EvaluationState.False, CheckResult:false, Operation.And,  currentResult:true,  ResolvesTo:false && true)],
+        [(EvaluationState.Pending | EvaluationState.False, CheckResult:false, Operation.And,  currentResult:false, ResolvesTo:false && false)],
+        [(EvaluationState.Pending | EvaluationState.False, CheckResult:false, Operation.Must, currentResult:true,  ResolvesTo:false && true)],
+        [(EvaluationState.Pending | EvaluationState.False, CheckResult:false, Operation.Must, currentResult:false, ResolvesTo:false && false)],
+    ];
+
+    internal static IEnumerable<object[]> StateResolutions =
+    [
+        [(Operation.Or,   BooleanResolutionResult:true, Yields:EvaluationState.True | EvaluationState.Pending)],
+        [(Operation.And,  BooleanResolutionResult:true, Yields:EvaluationState.True | EvaluationState.Pending)],
+        [(Operation.Must, BooleanResolutionResult:true, Yields:EvaluationState.True | EvaluationState.Pending)],
+
+        [(Operation.Or,   BooleanResolutionResult:false, Yields:EvaluationState.False | EvaluationState.Pending)],
+        [(Operation.And,  BooleanResolutionResult:false, Yields:EvaluationState.False | EvaluationState.Pending)],
+        [(Operation.Must, BooleanResolutionResult:false, Yields:EvaluationState.False | EvaluationState.Determined)]
+    ];
+}
---
+++ EvaluationTests/IResolverTests/IResolverTests.cs
@@ -0,0 +1,34 @@
+﻿using Evaluation.Internal;
+using Evaluation.Internal.Resolution;
+using Evaluation.Tests.IResolverTests.Data;
+
+
+namespace Evaluation.Tests.IResolverTests;
+
+public class IResolverTests
+{
+    public static IEnumerable<object[]> BooleanResolutions => ResolutionData.BooleanResolutions;
+    public static IEnumerable<object[]> StateResolutions => ResolutionData.StateResolutions;
+
+
+    [Theory]
+    [MemberData(nameof(BooleanResolutions))]
+    internal void BooleanResolution_ResolvesCorrectly
+    (
+        (EvaluationState CurrentState, bool CheckResult, Operation Operation, bool CurrentResult, bool ExpectedResolution) data
+    )
+    {
+        var result = IResolver.BooleanResolution(data.CurrentState, data.CheckResult, data.Operation, data.CurrentResult);
+
+        Assert.Equal(data.ExpectedResolution, result);
+    }
+
+    [Theory]
+    [MemberData(nameof(StateResolutions))]
+    internal void StateResolution_ResolvesCorrectly((Operation Operation, bool BooleanResolutionResult, EvaluationState ExpectedState) data)
+    {
+        var yieldedState = IResolver.StateResolution(data.BooleanResolutionResult, data.Operation);
+
+        Assert.Equal(data.ExpectedState, yieldedState);
+    }
+}
