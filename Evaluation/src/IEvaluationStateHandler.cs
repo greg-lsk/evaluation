0392879188e8670ade4a69576b330f05401c5b48
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-07 14:12:51 +0200

    update:: new definitions to 'IEvaluationHandler'
    > new property 'IsDetermined'
    > new property 'State'
    > refactored method 'DetermineNextState(...)'

--- Evaluation/src/IEvaluationStateHandler.cs
+++ Evaluation/src/IEvaluationStateHandler.cs
@@ -6,8 +6,11 @@ namespace Evaluation;
 public interface IEvaluationStateHandler<T> where T : struct
 {
     internal bool Result { get; }
+    internal bool IsDetermined { get; }
+    internal EvaluationState State { get; }
+
     internal T Terminate();
     internal T WithState(EvaluationState state);
 
-    internal EvaluationState DetermineNextState(bool evaluatedCheckResult);
+    internal EvaluationState DetermineNextState(Operation operation, bool evaluatedCheckResult);
 }
