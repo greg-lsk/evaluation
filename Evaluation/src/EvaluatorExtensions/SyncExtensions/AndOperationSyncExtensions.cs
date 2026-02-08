0aed7d97b4949136e5cba5530f2ddcda20c5f349
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-08 11:32:51 +0200

    new:: Evaluator extension methods added for 'Operation.And'
    > added support for: ['Check', 'Check<T>', 'CheckOnStruct<T>']
    > no support for: ['CheckOnView<T>']

---
+++ Evaluation/src/EvaluatorExtensions/SyncExtensions/AndOperationSyncExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Evaluation.Internal;
+
+
+namespace Evaluation;
+
+public static partial class EvaluationExtensions
+{
+    public static E And<E>(this E evaluator, Check check) 
+        where E : struct, IEvaluationStateHandler<E>
+    {
+        if (evaluator.IsDetermined) return evaluator;
+
+        var checkResult = check();
+        var state = evaluator.DetermineNextState(Operation.And, checkResult);
+
+        return evaluator.WithState(state);
+    }
+
+    public static E And<E, D>(this E evaluator, Check<D> check, D data) 
+        where E : struct, IEvaluationStateHandler<E>
+        where D : class
+    {
+        if (evaluator.IsDetermined) return evaluator;
+
+        var checkResult = check(data);
+        var state = evaluator.DetermineNextState(Operation.And, checkResult);
+
+        return evaluator.WithState(state);
+    }
+
+    public static E And<E, D>(this E evaluator, CheckOnStruct<D> check, in D data)
+        where E : struct, IEvaluationStateHandler<E>
+        where D : struct
+    {
+        if (evaluator.IsDetermined) return evaluator;
+
+        var checkResult = check(in data);
+        var state = evaluator.DetermineNextState(Operation.And, checkResult);
+
+        return evaluator.WithState(state);
+    }
+}
