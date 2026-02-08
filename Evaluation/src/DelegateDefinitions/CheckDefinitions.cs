757529859012f0f29f302861d5f46439b507755e
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-08 10:58:29 +0200

    style:: folder structure changed (no namespace adjustments)

--- Evaluation/src/DelegateDefinitions/CheckDefinitions.cs
+++ Evaluation/src/DelegateDefinitions/CheckDefinitions.cs
@@ -4,17 +4,16 @@
 namespace Evaluation;
 
 public delegate bool Check();
-public delegate Task<bool> CheckAsync();
-public delegate ValueTask<bool> CheckMaybeAsync();
-
 public delegate bool Check<T>(T data) where T : class;
-public delegate Task<bool> CheckAsync<T>(T data) where T : class;
-public delegate ValueTask<bool> CheckMaybeAsync<T>(T data) where T : class;
-
 public delegate bool CheckOnStruct<T>(in T data) where T : struct;
-public delegate Task<bool> CheckOnStructAsync<T>(T data) where T : struct;
-public delegate ValueTask<bool> CheckOnStructMaybeAsync<T>(T data) where T : struct;
-
 public delegate bool CheckOnView<T>(View<T> data) where T : struct;
+
+public delegate Task<bool> CheckAsync();
+public delegate Task<bool> CheckAsync<T>(T data) where T : class;
+public delegate Task<bool> CheckOnStructAsync<T>(T data) where T : struct;
 public delegate Task<bool> CheckOnViewAsync<T>(View<T> data) where T : struct;
+
+public delegate ValueTask<bool> CheckMaybeAsync();
+public delegate ValueTask<bool> CheckMaybeAsync<T>(T data) where T : class;
+public delegate ValueTask<bool> CheckOnStructMaybeAsync<T>(T data) where T : struct;
 public delegate ValueTask<bool> CheckOnViewMaybeAsync<T>(View<T> data) where T : struct;
