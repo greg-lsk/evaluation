58b7f8ddb3bf963e8d41efbf6956b54a23f43308
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-09 23:17:23 +0200

    new:: 'ResolutionDataHolder' class.
    > Used to seperate theory data declaration from 'LifecycleTests' class.

---
+++ EvaluationTests/IEvaluationStateHandler/Internals/ResolutionDataHolder.cs
@@ -0,0 +1,15 @@
+﻿using Evaluation.Tests.Common.Data;
+using Evaluation.Tests.IEvaluationStateHandler.Data;
+
+
+namespace Evaluation.Tests.IEvaluationStateHandler.Internals;
+
+public abstract class ResolutionDataHolder
+{
+    public static IEnumerable<object[]> ValidInitializedStates => EvaluationStates.ValidInitializedStates;
+    public static IEnumerable<object[]> ValidTerminationTransitions => EvaluationStates.ValidTerminationTransitions;
+    public static IEnumerable<object[]> StateResolutionForUninitializedEvaluators => EvaluationStates.ResolvedStatesForUninitializedEvaluators;
+
+    public static TheoryData<(EvaluationState CurrentState, Operation, bool CheckResult, EvaluationState ExpectedState)> 
+        StateResolutionsOnPendingEvaluator => StateResolutions.AllOperationResolutions;
+}
--- EvaluationTests/IEvaluationStateHandler/LifecycleTests.cs
+++ EvaluationTests/IEvaluationStateHandler/LifecycleTests.cs
@@ -1,20 +1,11 @@
 ﻿using Evaluation.Enums.Internals;
-using Evaluation.Tests.Common.Data;
-using Evaluation.Tests.IEvaluationStateHandler.Data;
+using Evaluation.Tests.IEvaluationStateHandler.Internals;
 
 
 namespace Evaluation.Tests.IEvaluationStateHandler;
 
-public class LifecycleTests
+public class LifecycleTests : ResolutionDataHolder
 {
-    public static IEnumerable<object[]> ValidInitializedStates => EvaluationStates.ValidInitializedStates;
-    public static IEnumerable<object[]> ValidTerminationTransitions => EvaluationStates.ValidTerminationTransitions;
-
-    public static IEnumerable<object[]> StateResolutionForUninitializedEvaluators => EvaluationStates.ResolvedStatesForUninitializedEvaluators;
-    public static TheoryData<(EvaluationState CurrentState, Operation, bool CheckResult, EvaluationState ExpectedState)> StateResolution 
-        => StateResolutions.AllOperationResolutions; 
-
-
     [Fact]
     internal void Create_Returns_AnInstance_WithUninitializedState()
     {
@@ -72,7 +63,7 @@ public class LifecycleTests
     }
 
     [Theory]
-    [MemberData(nameof(StateResolution))]
+    [MemberData(nameof(StateResolutionsOnPendingEvaluator))]
     internal void DetermineNextState_ReturnsCorrectly_ForInitializedEvaluators(
         (EvaluationState CurrectState, Operation Operation, bool CheckResult, EvaluationState ExpectedState) data)
     {
