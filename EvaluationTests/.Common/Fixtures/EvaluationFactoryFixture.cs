79d20eb5a3475ee1baaa39c4720768d75183b99e
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-10 00:03:34 +0200

    new:: 'EvaluationFactoryFixture' created

---
+++ EvaluationTests/.Common/Fixtures/EvaluationFactoryFixture.cs
@@ -0,0 +1,11 @@
+﻿namespace Evaluation.Tests.Common.Fixtures;
+
+public class EvaluationFactoryFixture<E> where E : struct, IEvaluationStateHandler<E>
+{
+    public Func<IEvaluationStateHandler<E>> CreateUninitialized => () => IEvaluationStateHandler<E>.Create();
+    public Func<EvaluationState, IEvaluationStateHandler<E>> CreateWithState => s =>
+    {
+        var evaluation = IEvaluationStateHandler<E>.Create();
+        return evaluation.WithState(s);
+    };
+}
