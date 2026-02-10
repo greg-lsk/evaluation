a09a3022d58eb8c525b112f2c08fb001e5d1329b
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-10 20:32:49 +0200

    update:: Async 'CheckStub' methods for future use.

--- EvaluationTests/EvaluationExtensions/.Stubs/CheckStub.cs
+++ EvaluationTests/EvaluationExtensions/.Stubs/CheckStub.cs
@@ -10,4 +10,13 @@ internal static class CheckStub
 
     internal static bool ChecksTrue<T>(in T data) where T : struct => true;
     internal static bool ChecksFalse<T>(in T data) where T : struct => false;
+
+    internal static async Task<bool> ChecksTrueAsync() { await Task.Delay(1000); return true; }
+    internal static async Task<bool> ChecksFalseAsync() { await Task.Delay(1000); return false; }
+
+    internal static async Task<bool> ChecksTrueOnRefAsync<T>(T data) where T: class { await Task.Delay(1000); return true; }
+    internal static async Task<bool> ChecksFalseOnRefAsync<T>(T data) where T : class { await Task.Delay(1000); return false; }
+
+    internal static async Task<bool> ChecksTrueOnValueAsync<T>(T data) where T : struct { await Task.Delay(1000); return true; }
+    internal static async Task<bool> ChecksFalseOnValueAsync<T>(T data) where T : struct { await Task.Delay(1000); return false; }
 }
