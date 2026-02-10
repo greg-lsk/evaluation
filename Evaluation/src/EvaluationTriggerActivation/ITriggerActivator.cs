b0a2ec696e38950b05afc5a9963d730ce5f05754
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-11 01:46:21 +0200

    update:: Method definitions.

---
+++ Evaluation/src/EvaluationTriggerActivation/ITriggerActivator.cs
@@ -0,0 +1,7 @@
+﻿namespace Evaluation;
+
+public interface ITriggerActivator
+{
+    internal void ActivateTriggersOn(Report report);
+    internal void ActivateTriggersOn<TData>(Report<TData> report);
+}
--- Evaluation/src/ITriggerActivator.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Evaluation;
-
-internal interface ITriggerActivator
-{
-    internal void ActivateFor<R>(R rule) where R : Delegate;
-    internal void ActivateFor<R, T>(R rule, T data) where R : Delegate;
-}
