2468d719038db93f5542b510b5edf189cb6d9b5a
Greg Liaskas <greg.liaskas@outlook.com>
2026-02-11 02:18:42 +0200

    update:: 'Evaluation' struct logic partialized.
    > Partialized 'IEvaluationStateHandler<...>' logic from Evaluation "core" members.

--- Evaluation/src/Evaluation.cs
+++ Evaluation/src/Evaluation.cs
@@ -1,40 +1,13 @@
 ﻿using Evaluation.Enums.Internals;
-using Evaluation.Internal.Resolution;
 
 
 namespace Evaluation;
 
-public readonly partial struct Evaluation : IEvaluationStateHandler<Evaluation>
+public readonly partial struct Evaluation
 {
     private readonly EvaluationState _state;
     private bool Result => _state.Is(EvaluationState.True);
 
-    bool IEvaluationStateHandler<Evaluation>.Result => Result;
-    bool IEvaluationStateHandler<Evaluation>.IsDetermined => _state.Is(EvaluationState.Determined);
-    EvaluationState IEvaluationStateHandler<Evaluation>.State => _state;
-
-
     public Evaluation() { _state = EvaluationState.Uninitialized; }
     private Evaluation(EvaluationState state) { _state = state; }
-
-    Evaluation IEvaluationStateHandler<Evaluation>.WithState(EvaluationState state) => new(state);
-    Evaluation IEvaluationStateHandler<Evaluation>.Terminate() => Result switch
-    {
-        true =>  new(EvaluationState.True | EvaluationState.Determined),
-        false => new(EvaluationState.False | EvaluationState.Determined)
-    };
-
-
-    EvaluationState IEvaluationStateHandler<Evaluation>.DetermineNextState(Operation operation, bool checkResult)
-    {
-        if (IResolver.EvaluationBecomesDetermined(checkResult, operation)) return EvaluationState.False | EvaluationState.Determined;
-
-        var state = _state.IsInitialized() switch
-        {
-            true =>  IResolver.Resolve(checkResult, operation, Result) ? EvaluationState.True : EvaluationState.False,
-            false => checkResult ? EvaluationState.True : EvaluationState.False
-        };
-
-        return state | EvaluationState.Pending;
-    }
 }
---
+++ Evaluation/src/EvaluationStateHandling/Evaluation.cs
@@ -0,0 +1,34 @@
+﻿using Evaluation.Enums.Internals;
+using Evaluation.Internal.Resolution;
+
+
+namespace Evaluation;
+
+public readonly partial struct Evaluation : IEvaluationStateHandler<Evaluation>
+{
+    bool IEvaluationStateHandler<Evaluation>.Result => Result;
+    bool IEvaluationStateHandler<Evaluation>.IsDetermined => _state.Is(EvaluationState.Determined);
+    EvaluationState IEvaluationStateHandler<Evaluation>.State => _state;
+
+
+    Evaluation IEvaluationStateHandler<Evaluation>.WithState(EvaluationState state) => new(state);
+    Evaluation IEvaluationStateHandler<Evaluation>.Terminate() => Result switch
+    {
+        true =>  new(EvaluationState.True | EvaluationState.Determined),
+        false => new(EvaluationState.False | EvaluationState.Determined)
+    };
+
+
+    EvaluationState IEvaluationStateHandler<Evaluation>.DetermineNextState(Operation operation, bool checkResult)
+    {
+        if (IResolver.EvaluationBecomesDetermined(checkResult, operation)) return EvaluationState.False | EvaluationState.Determined;
+
+        var state = _state.IsInitialized() switch
+        {
+            true =>  IResolver.Resolve(checkResult, operation, Result) ? EvaluationState.True : EvaluationState.False,
+            false => checkResult ? EvaluationState.True : EvaluationState.False
+        };
+
+        return state | EvaluationState.Pending;
+    }
+}
